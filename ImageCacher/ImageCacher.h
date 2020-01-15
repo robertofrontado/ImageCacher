ce494a096cb2a4a8f38d0acb091d0aa877b70199
Roberto Frontado <robertofrontado@gmail.com>
2020-01-15 19:16:56 +0100

    Initial Commit

---
+++ ImageCacher/ImageCacher.h
@@ -0,0 +1,19 @@
+//
+//  ImageCacher.h
+//  ImageCacher
+//
+//  Created by Roberto Frontado on 15/01/2020.
+//  Copyright © 2020 Roberto Frontado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageCacher.
+FOUNDATION_EXPORT double ImageCacherVersionNumber;
+
+//! Project version string for ImageCacher.
+FOUNDATION_EXPORT const unsigned char ImageCacherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCacher/PublicHeader.h>
+
+
