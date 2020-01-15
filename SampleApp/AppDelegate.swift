4ef3854c8ac44bdbe572aea5f70f07cd52d95939
Roberto Frontado <robertofrontado@gmail.com>
2020-01-15 19:20:03 +0100

    Added Empty project

---
+++ SampleApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SampleApp
+//
+//  Created by Roberto Frontado on 15/01/2020.
+//  Copyright © 2020 Roberto Frontado. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Roberto Frontado on 15/01/2020.
+//  Copyright © 2020 Roberto Frontado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
